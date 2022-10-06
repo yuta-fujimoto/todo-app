d489c7b5669cd0117e1a372f20a31bd3e98542bc
yuta-fujimoto <fyuta@student.42tokyo.jp>
2022-10-06 16:51:38 +0900

    first commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [PrismaModule, JwtModule.register({})],  
+  controllers: [AuthController],
+  providers: [AuthService]
+})
+export class AuthModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { IsOptional, IsString } from "class-validator";
+import { PrismaService } from "src/prisma/prisma.service";
+import { UpdateUserDto } from "./dto/update-user.dto";
+
+@Injectable()
+export class UserService {
+    constructor(private prisma: PrismaService) {}
+
+    async updateUser(
+        userId: number,
+        dto: UpdateUserDto
+    ): Promise<Omit<User, 'hashedPassword'>> {
+        const user = await this.prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                ...dto,
+            },
+        });
+        delete user.hashedPassword;
+        return user
+    }
+}
